81966f8ade408d1604be0518ab3870cbcbd76935
Naman Gupta <01namangupta@gmail.com>
2017-12-18 20:38:47 +0530

    Devise is implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
+	root to: "home#index"
+	
+  	get 'home/index'
 
-  get 'home/campaign'
+  	get 'home/campaign'
 
-  get 'home/new_campaign_user'
+  	get 'home/new_campaign_user'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
